1009e11842854375e701ab4ae9ca6e8644c4546f
Victor <victorsancheznogueira@gmail.com>
2025-02-13 09:51:57 +0100

    feat: filtrado de libros

--- app/src/main/java/com/example/bibliotecateis/FiltradoLibros.java
+++ app/src/main/java/com/example/bibliotecateis/FiltradoLibros.java
@@ -8,8 +8,12 @@ import com.example.bibliotecateis.API.models.BookLending;
 import com.example.bibliotecateis.API.models.User;
 import com.example.bibliotecateis.API.repository.BookRepository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class FiltradoLibros {
 
@@ -34,6 +38,60 @@ public class FiltradoLibros {
             });
         }
         return prestados;
+    }
+
+//    Opcionalmente, cuando se muestren los libros disponibles se debería poder ver un
+//    indicador de cuantas copias existen en total y cuantas están disponibles
+
+    public int copiasRestantes(Book book){
+        List<Book> prestados = new ArrayList<>();
+        for(BookLending bookLending : user.getBookLendings()){
+            bookRepository.getBookById(bookLending.getBookId(),new BookRepository.ApiCallback<Book>() {
+
+                @Override
+                public void onSuccess(Book result) {
+                    prestados.add(result);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.e("BookRepository", "Error deleting book", t);
+                }
+            });
+        }
+        return prestados;
+    }
+
+    public String fechaMasProxima(Book book){
+        String ultimaFecha = "";
+        for(BookLending bookLending : book.getBookLendings()){
+            ultimaFecha = obtenerFechaMasAntigua(ultimaFecha, bookLending.getLendDate());
+        }
+        return ultimaFecha;
+    }
+
+    public static String obtenerFechaMasAntigua(String fecha1, String fecha2) {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        formatoFecha.setTimeZone(TimeZone.getTimeZone("UTC"));
+        if(fecha2.isEmpty()){
+            return fecha1;
+        }
+        if(fecha1.isEmpty()){
+            return fecha2;
+        }
+        try {
+            Date date1 = formatoFecha.parse(fecha1);
+            Date date2 = formatoFecha.parse(fecha2);
+
+            if (date1.before(date2)) {
+                return fecha1;
+            } else {
+                return fecha2;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
