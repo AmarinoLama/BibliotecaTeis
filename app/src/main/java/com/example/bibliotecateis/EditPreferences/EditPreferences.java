4ed4cd6056cd196bfcd29358eeff9207a23d7b90
Victor <Victorsancheznogueira@gmail.com>
2025-03-03 19:14:30 +0100

    feat: add toolbar to preferences

--- app/src/main/java/com/example/bibliotecateis/EditPreferences/EditPreferences.java
+++ app/src/main/java/com/example/bibliotecateis/EditPreferences/EditPreferences.java
@@ -1,9 +1,13 @@
 package com.example.bibliotecateis.EditPreferences;
 
+import static com.example.bibliotecateis.Helpers.cargarToolbar;
+
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -12,23 +16,26 @@ import com.example.bibliotecateis.R;
 
 public class EditPreferences extends AppCompatActivity {
 
+    private Toolbar tb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_preferences);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        tb = findViewById(R.id.toolbar);
+        cargarToolbar(this,tb);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new PreferenceFragment())
+                .replace(R.id.preferences_container, new PreferenceFragment())
                 .commit();
-
-
     }
 }
